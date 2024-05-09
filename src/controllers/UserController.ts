947ca9af02b24b25a69506b4b7047ed446a6f184
Marcelo <sjsjsjmarcelo@gmail.com>
2024-05-09 11:48:44 -0300

    feat: add suport update user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -42,4 +42,18 @@ export class UserController {
             return res.status(500).json({ message: "Internal server error" });
         }
     }
+
+    async update(req: Request, res: Response) {
+        const { name, password } = req.body;
+        const { id } = req.params;
+
+        const hashPassword = await hash(password, 8)
+
+        try {
+            const user = await prisma.user.update({ where: { id: Number(id) }, data: { name, password: hashPassword } });
+            return res.status(200).json(user);
+        } catch (error) {
+            return res.status(500).json({ message: "Internal server error" });
+        }
+    }
 }
